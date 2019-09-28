d215f8c2240860b942725fb6e7e05590a64dba4d
Gerard Rappa <gerard.rappa@gmail.com>
2019-09-28 13:43:04 -0700

    Edited code to enable testing at my home

--- MateriaShop/Models/FakeProductRepository.cs
+++ MateriaShop/Models/FakeProductRepository.cs
@@ -7,9 +7,31 @@ namespace MateriaShop.Models
     {
         public IQueryable<Product> Products => new List<Product>
         {
-            new Product { Name = "Fire", Category = "Magic", Description = "Fire based magic", Price = 250 },
-            new Product { Name = "Ice", Category = "Magic", Description = "Ice based magic", Price = 250 },
-            new Product { Name = "Restore", Category = "Magic", Description = "Healing magic", Price = 500 }
+            new Product{
+                        Name = "Fire",
+                        Description = "Fire based magic",
+                        Category = "Magic",
+                        Price = 250},
+                    new Product{
+                        Name = "Ice",
+                        Description = "Ice based magic",
+                        Category = "Magic",
+                        Price = 250},
+                    new Product{
+                        Name = "Restore",
+                        Description = "Healing magic",
+                        Category = "Magic",
+                        Price = 500},
+                    new Product{
+                        Name = "Ultima",
+                        Description = "Awesome magic",
+                        Category = "Magic",
+                        Price = 50000},
+                    new Product{
+                        Name = "Bahamut",
+                        Description = "Non-Elemental Summon",
+                        Category = "Summon",
+                        Price = 65000}
         }.AsQueryable<Product>();
     }
 }
--- MateriaShop/Startup.cs
+++ MateriaShop/Startup.cs
@@ -26,6 +26,7 @@ namespace MateriaShop
             services.AddDbContext<ApplicationDbContext>(option => 
                 option.UseSqlServer(Configuration["Data:MateriaShopProducts:ConnectionString"]));
             services.AddTransient<IProductRepository, EFProductRepository>();
+            //services.AddTransient<IProductRepository, FakeProductRepository>();
             services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddTransient<IOrderRepository, EFOrderRepository>();
